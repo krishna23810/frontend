7c59e4fa4f7916c1b327bde6b7df14a474b7fa53
krishna23810 <85396898+krishna23810@users.noreply.github.com>
2025-09-09 09:56:45 +0530

    Update apiconnector.js

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 export const axiosInstance = axios.create({
     withCredentials: true, // This enables sending cookies
     // baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/api'
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/api'
-
+    baseURL: process.env.REACT_APP_BASE_URL  || 'http://localhost:3000/api'
+// REACT_APP_BASE_URL 
 });
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
