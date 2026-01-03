0fbc34b3c46b9505c43bfd24dd32d8647a334aa6
krishna23810 <85396898+krishna23810@users.noreply.github.com>
2026-01-04 00:08:27 +0530

    Update profileDropDown.jsx

--- src/components/comman/profileDropDown.jsx
+++ src/components/comman/profileDropDown.jsx
@@ -11,14 +11,17 @@ const ProfileDropDown = () => {
     const dispatch = useDispatch();
     const handleLogout = () => {
         // Perform logout logic here
-        dispatch(logout());
+        dispatch(logout(navigate));
     };
 
     return (
         <div className="absolute -right-0 mt-2 w-30 top-8 bg-white text-gray-600 border border-gray-300 rounded-md shadow-lg z-10">
             <ul className="py-2">
-                <button onClick={() => navigate("/dashboard/my-profile")}>
+                <button onClick={() => navigate("/dashboard/enrolled-courses")}>
                 <li className="px-4 py-2 hover:bg-gray-100">Dashbord</li>
+                </button>
+                <button onClick={() => navigate("/dashboard/my-profile")}>
+                <li className="px-4 py-2 hover:bg-gray-100">Profile</li>
                 </button>
                 <button onClick={handleLogout}>
                 <li className="px-4 py-2 hover:bg-gray-100">Logout</li>
